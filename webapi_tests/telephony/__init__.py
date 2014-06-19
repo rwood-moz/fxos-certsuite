7d3bc5ce519d9d2927ed88b9c342ad0a8e3e9d63
rwood-moz <rwood@mozilla.com>
2014-06-19 15:28:16 -0400

    Bug 1023345 - 1.4 semiauto webapi test updates

--- webapi_tests/telephony/__init__.py
+++
@@ -1,7 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this file,
-# You can obtain one at http://mozilla.org/MPL/2.0/.
-
-from webapi_tests.telephony.telephony_test import TelephonyTestCommon
-from webapi_tests.telephony.test_telephony_incoming import TestTelephonyIncoming
-from webapi_tests.telephony.test_telephony_outgoing import TestTelephonyOutgoing
